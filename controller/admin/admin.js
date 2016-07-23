6716041c3adff151235013320d5a52598999d522
TracyWalt <1443299799@qq.com>
2016-07-23 14:46:32 +0800

    添加后台管理登录注册功能

---
+++ controller/admin/admin.js
@@ -0,0 +1,62 @@
+/**
+ * Author: walt
+ * Time: 2016/7/23
+ */
+var User = require('../../models/admin/user');
+
+//后台管理登录页
+exports.login = function(req,res){
+    res.render('admin/login',{});
+}
+
+//后台管理首页
+exports.index = function(req,res){
+    if(req.session.admin){  //如果已经登录
+        res.render('admin/index',{});
+    }else{
+        res.redirect('/admin/login');
+    }
+}
+
+//处理后台管理登录
+exports.singin = function(req,res){
+
+    var username = req.body.username;  //获取提交过来的用户名
+    var password = req.body.password;  //获取提交过来的密码
+
+    if(username && password){
+        var userdata = {username:username,password:password};
+        //先去查找数据库有没有注册过管理员，有则判断用户名密码是否正确，没有就直接注册一个新用户
+        User.find({},function(err,docs){
+
+            if(docs.length){
+                //已经注册过管理员帐号
+                User.find(userdata,function(err,docs){
+                    if(docs.length){  //登录成功
+                        req.session.admin = username;
+                        res.redirect('/admin');
+                    }else{
+                        res.render('admin/login',{});
+                    }
+                });
+            }else{
+                //注册新管理帐号
+                var user = new User(userdata);
+                user.save(function(err){
+                    if(err){
+                        console.log('注册失败');
+                    }else{
+                        console.log('注册成功');
+                        req.session.admin = username;
+                        res.redirect('/admin');
+                    }
+                });
+            }
+
+        });
+
+
+    }else{
+        res.render('admin/login',{});
+    }
+}
--- router/router.js
+++ router/router.js
@@ -4,9 +4,15 @@
  */
 var express = require('express');
 var router = express.Router();
+var adminController = require('../controller/admin/admin');
 
 router.get('/',function(req,res){
-    res.send('aa');
+    res.send('首页');
 });
+
+//admin router
+router.get('/admin',adminController.index);
+router.get('/admin/login',adminController.login);
+router.post('/admin/singin',adminController.singin);
 
 module.exports = router;
