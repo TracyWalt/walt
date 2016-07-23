d3f5400c457200ef5eb6ef84e5e25b7c2309d53b
TracyWalt <1443299799@qq.com>
2016-07-23 11:37:39 +0800

    init目录

---
+++ app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2016/7/23.
+ */
+var express = require('express');
+var app = express();
+var router = require('./router/router'); //路由
+
+//路由
+app.use(router);
+
+app.listen(8000,function(){
+   console.log('server start...');
+});
---
+++ router/router.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/7/23.
+ */
+var express = require('express');
+var router = express.Router();
+
+router.get('/',function(req,res){
+    res.send('aa');
+});
+
+module.exports = router;
